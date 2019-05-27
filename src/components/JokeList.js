32d40076ea8d5cdae00c3a906b6facb1ac8e596a
Artas <artasss47@gmail.com>
2019-05-27 15:32:44 +0200

    saving jokes in local storage

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -3,6 +3,7 @@ import jokess from "../apis/jokes";
 import "./JokeList.scss";
 import Joke from "./Joke";
 import uuid from "uuid/v4";
+import { async } from "q";
 
 export default class JokeList extends Component {
   static defaultProps = {
@@ -10,15 +11,24 @@ export default class JokeList extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { jokes: [] };
+    this.state = {
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+    };
   }
   componentDidMount = async () => {
+    if (this.state.jokes.length === 0) {
+      this.getJokes();
+    }
+  };
+
+  getJokes = async () => {
     let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
       let res = await jokess.get();
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
     this.setState({ jokes: jokes });
+    window.localStorage.setItem("jokes", JSON.stringify(jokes));
   };
 
   handleVote = (id, delta) => {
@@ -48,6 +58,7 @@ export default class JokeList extends Component {
         {console.log("joki", this.state.jokes.text)}
         <div className="jokes__sidebar">
           <h1 className="jokes__title">Jokes</h1>
+          <button className="jokes__button">Get new jokes</button>
         </div>
         <ul className="jokes__list">{this.renderJokes()}</ul>
       </div>
